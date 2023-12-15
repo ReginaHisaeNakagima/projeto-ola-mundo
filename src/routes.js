b0d38cc32994c167933cf29a30180e987fa7e6a0
HisaeNakagima <1823289@aluno.univesp.br>
2023-12-15 17:05:52 -0300

    Simplificando caminhos

--- src/routes.js
+++ src/routes.js
@@ -12,21 +12,9 @@ function AppRoutes() {
 
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/sobremim" element={<SobreMim />} />
+          <Route index element={<Inicio />} />
+          <Route path="sobremim" element={<SobreMim />} />
         </Route>
-
-        {/* 
-        Na rota "/", estrutura a ser renderizada é:
-        <PaginaPadrao>
-          <Inicio />
-        </PaginaPadrao>
-
-        Na rota "/sobremim", estrutura a ser renderizada é:
-        <PaginaPadrao>
-          <SobreMim />
-        </PaginaPadrao>
-        */}
 
         <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
